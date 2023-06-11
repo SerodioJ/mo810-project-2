8b3207d82db794fb8bf81286d28c2f75614ca03c
João Seródio <joaoserodio@live.com>
2023-06-11 01:50:04 -0300

    Initial Implementation

---
+++ run_model.py
@@ -0,0 +1,150 @@
+import argparse
+from pathlib import Path
+from time import perf_counter
+
+
+from dasf.ml.xgboost import XGBRegressor
+from dasf.pipeline import Pipeline
+from dasf.pipeline.executors import DaskPipelineExecutor
+from dasf.utils.funcs import is_gpu_supported
+
+from utils import (
+    ZarrDataset,
+    FeaturesJoin,
+    ReshapeFeatures,
+    SaveResult,
+    generate_neighbourhood_features,
+    create_executor,
+)
+
+
+def load_model(fname):
+    xgboost = XGBRegressor()
+    xgboost.__xgb_cpu.load_model(fname)
+    xgboost.__xgb_mcpu.load_model(fname)
+    if is_gpu_supported():
+        xgboost.__xgb_gpu.load_model(fname)
+        xgboost.__xgb_mgpu.load_model(fname)
+    return xgboost
+
+
+def create_pipeline(
+    executor: DaskPipelineExecutor,
+    dataset_path: Path,
+    ml_model: Path,
+    inline_window: int,
+    trace_window: int,
+    samples_window: int,
+    output: str,
+    pipeline_save_location: str,
+) -> Pipeline:
+
+    dataset = ZarrDataset(name="F3 dataset", data_path=dataset_path)
+
+    neighbourhood = generate_neighbourhood_features(
+        inline_window, trace_window, samples_window
+    )
+
+    features_join = FeaturesJoin()
+    reshape_features = ReshapeFeatures()
+    xgboost = load_model(ml_model)
+    save_result = SaveResult(output)
+
+    pipeline = Pipeline(
+        name=f"XGBoost({ml_model}) Inference Pipeline", executor=executor
+    )
+    pipeline.add(dataset)
+    for neighbour in neighbourhood.values():
+        pipeline.add(neighbour, X=dataset)
+    pipeline.add(features_join, **{"(i,j,k)": dataset, **neighbourhood})
+    pipeline.add(reshape_features, X=features_join)
+    pipeline.add(xgboost.predict, X=reshape_features)
+    pipeline.add(save_result, X=xgboost.predict)
+
+    if pipeline_save_location is not None:
+        pipeline.visualize(filename=pipeline_save_location)
+
+    return pipeline
+
+
+def run_model(args):
+    executor = create_executor(args.address)
+    print("Creating pipeline...")
+    pipeline = create_pipeline(
+        executor,
+        args.data,
+        args.ml_model,
+        args.inline_window,
+        args.trace_window,
+        args.samples_window,
+        args.output,
+        args.fig_pipeline,
+    )
+
+    print("Executing pipeline...")
+    start = perf_counter()
+    pipeline.run()
+    end = perf_counter()
+
+    print(f"Done! Execution time: {end - start:.2f} s")
+    return end - start
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "-m", "--ml-model", help="trained model file", type=Path, required=True
+    )
+
+    parser.add_argument(
+        "-d",
+        "--data",
+        help="path to input seismic data",
+        type=Path,
+        default="data/F3_train.zarr",
+    )
+
+    parser.add_argument(
+        "-i",
+        "--inline-window",
+        help="number of neighbors in inline dimension",
+        type=int,
+        default=0,
+    )
+
+    parser.add_argument(
+        "-t",
+        "--trace-window",
+        help="number of neighbors in trace dimension",
+        type=int,
+        default=0,
+    )
+
+    parser.add_argument(
+        "-s",
+        "--samples-window",
+        help="number of neighbors in samples dimension",
+        type=int,
+        default=0,
+    )
+
+    parser.add_argument(
+        "-e",
+        "--address",
+        help="Dask Scheduler address HOST:PORT",
+        type=str,
+        default=None,
+    )
+
+    parser.add_argument(
+        "-o",
+        "--output",
+        help="name of output file to save generated seismic aatribute",
+        type=str,
+        required=True,
+    )
+
+    args = parser.parse_args()
+
+    run_model(args)
---
+++ utils.py
@@ -0,0 +1,138 @@
+import dask.array as da
+import numpy as np
+import zarr
+
+from dasf.transforms import Transform
+from dasf.datasets import Dataset
+from dasf.pipeline.executors import DaskPipelineExecutor
+from dasf.utils.decorators import task_handler
+
+
+class ZarrDataset(Dataset):
+    def __init__(self, name: str, data_path: str, chunks: str = "32Mb"):
+        super().__init__(name=name)
+        self.data_path = data_path
+        self.chunks = chunks
+
+    def _lazy_load_cpu(self):
+        return da.from_zarr(self.data_path, chunks=self.chunks)
+
+    def _load_cpu(self):
+        return zarr.load(self.data_path)
+
+    @task_handler
+    def load(self):
+        ...
+
+
+class NeighbourValue(Transform):
+    def __init__(self, dim: int, pos: int):
+        self._dim = dim
+        self._pos = pos
+
+    def __pad_width(self, dims):
+        paddings = []
+        for i in range(dims):
+            if i != self._dim:
+                paddings.append((0, 0))
+                continue
+            if self._pos > 0:
+                paddings.append((0, self._pos))
+            else:
+                paddings.append((np.abs(self._pos), 0))
+        return paddings
+
+    def __neighbours(self, X, xp):
+        return xp.roll(X, shift=-self._pos, axis=self._dim)
+
+    def _lazy_transform_cpu(self, X):
+        depth = [0] * len(X.shape)
+        depth[self._dim] = np.abs(self._pos)
+        return X.map_overlap(
+            self.__neighbours,
+            xp=np,
+            depth=tuple(depth),
+            boundary="nearest",
+            meta=np.array(()),
+        )
+
+    def _transform_cpu(self, X):
+        # X = np.pad(X, self.__pad_width(len(X.shape)), mode="edge")
+        # TO DO
+        return X
+
+
+class FeaturesJoin(Transform):
+    def _lazy_transform_cpu(self, **features):
+        return da.stack(features.values(), axis=-1)
+
+    def _transform_cpu(self, **features):
+        return np.stack(features.values(), axis=-1)
+
+
+class ReshapeFeatures(Transform):
+    def _lazy_transform_cpu(self, X):
+        X = X.reshape(-1, X.shape[-1])
+        X = X.rechunk({0: "auto", 1: -1})
+        return X
+
+    def _transform_cpu(self, X):
+        return X.reshape(-1, X.shape[-1])
+
+
+class ReshapeLabels(Transform):
+    def _lazy_transform_cpu(self, X, y):
+        y = y.flatten()
+        y = y.rechunk(X.chunks[:-1])
+        print(f"X: {X.chunks} - {X.chunksize}")
+        print(f"y: {y.chunks} - {y.chunksize}")
+        return y
+
+    def _transform_cpu(self, X, y):
+        return y.flatten()
+
+
+class SaveModel(Transform):
+    def __init__(self, fname: str):
+        self._fname = fname
+
+    def transform(self, model):
+        model.save_model(self._fname)
+
+
+class SaveResult(Transform):
+    def __init__(self, fname: str):
+        self._fname = fname
+
+    def _lazy_transform_cpu(self, X):
+        zarr.save(self._fname, X.compute())
+
+    def _transform_cpu(self, X):
+        zarr.save(self._fname, X)
+
+
+def generate_neighbourhood_features(
+    inline_window, trace_window, samples_window
+) -> dict:
+    def feature_name(dim, pos):
+        name = ["i", "j", "k"]
+        name[dim] = f"{name[dim]}{'+' if pos > 0 else '-'}{np.abs(pos)}"
+        return f"({','.join(name)})"
+
+    operators = {}
+    for dim, n in zip([2, 1, 0], [samples_window, trace_window, inline_window]):
+        for pos in range(-n, n + 1):
+            if pos == 0:
+                continue
+            operators[feature_name(dim, pos)] = NeighbourValue(dim=dim, pos=pos)
+    return operators
+
+
+def create_executor(address: str = None) -> DaskPipelineExecutor:
+    if address is not None:
+        addr = ":".join(address.split(":")[:2])
+        port = str(address.split(":")[-1])
+        print(f"Creating executor. Address: {addr}, port: {port}")
+        return DaskPipelineExecutor(local=False, use_gpu=False, address=addr, port=port)
+    else:
+        return DaskPipelineExecutor(local=True, use_gpu=False)
