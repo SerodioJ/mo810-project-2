a128ac6a81b9cc8cafbdbaa68dd1f38855a09fce
João Seródio <joaoserodio@live.com>
2023-06-25 18:46:12 -0300

    update training and inference evalatuation scripts

--- evaluate_inference.py
+++ evaluate_inference.py
@@ -31,9 +31,11 @@ attributes = {
 }
 
 
-def run(run_args, attribute, time, r2):
-    time.value = run_model(run_args)
-    data = da.from_zarr(run_args.data, chunks={0: "auto",0: "auto", 0: -1})
+def run(run_args, attribute, time, save, r2):
+    predict, total = run_model(run_args)
+    time.value = predict
+    save.value = total
+    data = da.from_zarr(run_args.data, chunks={0: "auto", 0: "auto", 0: -1})
     attr = attributes[attribute]()
     y_hat = attr.transform(data)
     y_pred = zarr.load(run_args.output)
@@ -41,7 +43,9 @@ def run(run_args, attribute, time, r2):
 
 
 def get_models(workers, prefix):
-    return sorted(glob(os.path.join("models", str(workers), f"{prefix}-*.json")))
+    return sorted(
+        glob(os.path.join("models", str(4), f"rebalance_0-{prefix}-4-4-4.json"))
+    )
 
 
 def get_windows(model_file):
@@ -54,11 +58,14 @@ def get_windows(model_file):
         "samples_window": int(contents[-3]),
     }
 
+
 def model_name(prefix, workers, x, y, z, ext="png"):
     return os.path.join("models", str(workers), f"{prefix}-run-{x}-{y}-{z}.{ext}")
 
+
 def evaluate_inference(args):
-    pipeline_time = Value("d", 0)
+    predict_time = Value("d", 0)
+    save_time = Value("d", 0)
     r2 = Value("d", 0)
     for attr, prefix in models_prefix.items():
         with open(f"run-{attr}-{args.workers}.csv", "w") as f:
@@ -68,12 +75,16 @@ def evaluate_inference(args):
                     "inline_window",
                     "trace_window",
                     "samples_window",
-                    "pipeline_time",
+                    "predict_time",
+                    "save_time",
                     "process_time",
                     "r2_score",
                 ]
             )
             for model in get_models(args.workers, prefix):
+
+                if "chunks" in model:
+                    continue
                 print(model)
                 windows = get_windows(model)
                 run_args = {
@@ -89,29 +100,33 @@ def evaluate_inference(args):
                         y=windows["trace_window"],
                         z=windows["inline_window"],
                     ),
+                    "report": f"w{args.workers}-{attr}",
                     **windows,
                 }
                 run_args = namedtuple("args", run_args.keys())(*run_args.values())
-                pipeline_time.value = 0
-                r2.value = 0
-                p = Process(
-                    target=run,
-                    args=(run_args, attr, pipeline_time, r2),
-                )
-                start = perf_counter()
-                p.start()
-                p.join()
-                time = perf_counter() - start
-                writer.writerow(
-                    [
-                        windows["inline_window"],
-                        windows["trace_window"],
-                        windows["samples_window"],
-                        pipeline_time.value,
-                        time,
-                        r2.value,
-                    ]
-                )
+                for sample in range(3):
+                    predict_time.value = 0
+                    save_time.value = 0
+                    r2.value = 0
+                    p = Process(
+                        target=run,
+                        args=(run_args, attr, predict_time, save_time, r2),
+                    )
+                    start = perf_counter()
+                    p.start()
+                    p.join()
+                    time = perf_counter() - start
+                    writer.writerow(
+                        [
+                            windows["inline_window"],
+                            windows["trace_window"],
+                            windows["samples_window"],
+                            predict_time.value,
+                            save_time.value,
+                            time,
+                            r2.value,
+                        ]
+                    )
 
 
 if __name__ == "__main__":
--- evaluate_training.py
+++ evaluate_training.py
@@ -4,9 +4,10 @@ import os
 from collections import namedtuple
 from pathlib import Path
 from multiprocessing import Process, Value
-from time import perf_counter
+from time import perf_counter, sleep
 
-from train_model import train_model
+from train_model import train_model as train_model
+from train_model_rebalance import train_model as t_rebalance
 
 models_prefix = {
     # "ENVELOPE": "Env-ml-model",
@@ -16,82 +17,92 @@ models_prefix = {
 
 configs = [
     {"inline_window": 0, "trace_window": 0, "samples_window": 0},
-    {"inline_window": 0, "trace_window": 0, "samples_window": 1},
-    {"inline_window": 0, "trace_window": 0, "samples_window": 2},
-    {"inline_window": 0, "trace_window": 0, "samples_window": 3},
     {"inline_window": 0, "trace_window": 0, "samples_window": 4},
-    {"inline_window": 1, "trace_window": 1, "samples_window": 1},
-    # {"inline_window": 2, "trace_window": 2, "samples_window": 2},
-    # {"inline_window": 4, "trace_window": 4, "samples_window": 4},
-    # {"inline_window": 0, "trace_window": 0, "samples_window": 4},
+    {"inline_window": 2, "trace_window": 2, "samples_window": 2},
+    {"inline_window": 4, "trace_window": 4, "samples_window": 4},
 ]
 
+modules = {
+    "baseline": train_model,
+    "rebalance": t_rebalance,
+}
 
-def train(train_args, time):
-    time.value = train_model(train_args)
+
+def train(train_args, time, func):
+    time.value = func(train_args)
 
 
-def model_name(prefix, workers, x, y, z, ext="json"):
-    return os.path.join("models", str(workers), f"{prefix}-{x}-{y}-{z}.{ext}")
+def model_name(module, prefix, workers, x, y, z, ext="json"):
+    return os.path.join("models", str(workers), f"{module}-{prefix}-{x}-{y}-{z}.{ext}")
 
 
 def evaluate_training(args):
     pipeline_time = Value("d", 0)
-    for attr, prefix in models_prefix.items():
-        with open(f"train-{attr}-{args.workers}.csv", "w") as f:
-            writer = csv.writer(f)
-            writer.writerow(
-                [
-                    "inline_window",
-                    "trace_window",
-                    "samples_window",
-                    "pipeline_time",
-                    "process_time",
-                ]
-            )
-            for config in configs:
-                print(config)
-                train_args = {
-                    "attribute": attr,
-                    "fig_pipeline": None,
-                    "output": model_name(
-                        prefix=prefix,
-                        workers=args.workers,
-                        x=config["samples_window"],
-                        y=config["trace_window"],
-                        z=config["inline_window"],
-                    ),
-                    "data": args.data,
-                    "address": args.address,
-                    "fig_pipeline": model_name(
-                        prefix=prefix,
-                        workers=args.workers,
-                        x=config["samples_window"],
-                        y=config["trace_window"],
-                        z=config["inline_window"],
-                        ext="png",
-                    ),
-                    **config,
-                }
-                train_args = namedtuple("args", train_args.keys())(*train_args.values())
-                pipeline_time.value = 0
-                p = Process(
-                    target=train,
-                    args=(train_args, pipeline_time),
-                )
-                start = perf_counter()
-                p.start()
-                p.join()
-                time = perf_counter() - start
+    for module, func in modules.items():
+        for attr, prefix in models_prefix.items():
+            with open(f"train-{module}-{attr}-{args.workers}.csv", "w") as f:
+                writer = csv.writer(f)
                 writer.writerow(
                     [
-                        config["inline_window"],
-                        config["trace_window"],
-                        config["samples_window"],
-                        pipeline_time.value,
-                        time,
+                        "inline_window",
+                        "trace_window",
+                        "samples_window",
+                        "pipeline_time",
+                        "process_time",
                     ]
                 )
+                for config in configs:
+                    print(config)
+                    for sample in range(3):
+                        print(f"{sample} SAMPLE")
+                        train_args = {
+                            "attribute": attr,
+                            "fig_pipeline": None,
+                            "output": model_name(
+                                module=f"{module}_{sample}",
+                                prefix=prefix,
+                                workers=args.workers,
+                                x=config["samples_window"],
+                                y=config["trace_window"],
+                                z=config["inline_window"],
+                            ),
+                            "data": args.data,
+                            "address": args.address,
+                            "fig_pipeline": model_name(
+                                module=module,
+                                prefix=prefix,
+                                workers=args.workers,
+                                x=config["samples_window"],
+                                y=config["trace_window"],
+                                z=config["inline_window"],
+                                ext="png",
+                            ),
+                            "report": f"{module}-w{args.workers}.{sample}",
+                            "workers": args.workers,
+                            **config,
+                        }
+                        train_args = namedtuple("args", train_args.keys())(
+                            *train_args.values()
+                        )
+                        pipeline_time.value = 0
+                        p = Process(
+                            target=train,
+                            args=(train_args, pipeline_time, func),
+                        )
+                        sleep(10)
+                        start = perf_counter()
+                        p.start()
+                        p.join()
+                        time = perf_counter() - start
+                        writer.writerow(
+                            [
+                                config["inline_window"],
+                                config["trace_window"],
+                                config["samples_window"],
+                                pipeline_time.value,
+                                time,
+                            ]
+                        )
 
 
 if __name__ == "__main__":
@@ -115,7 +126,7 @@ if __name__ == "__main__":
         help="Dask Scheduler address HOST:PORT",
         type=str,
         required=False,
-        default=None
+        default=None,
     )
 
     args = parser.parse_args()
